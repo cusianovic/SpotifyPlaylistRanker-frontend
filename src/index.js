4b5955274e045b98a6a012f9e31cdf79e6f7ca90
cusianovic <scusianovic@gmail.com>
2023-10-28 14:13:36 -0700

    added auth

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css';
 import App from './App';
 
@@ -8,7 +9,14 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App/>
+      <Auth0Provider
+      domain="dev-43dibfgl0wzgvp8x.us.auth0.com"
+      clientId="ywUJMUpwwehEWs1yO3smHGhGgehkEFhg"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}>
+        <App/>
+      </Auth0Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
