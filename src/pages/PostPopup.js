71782cdf295df192393203838de15ba5b87e8c6d
Austin Tung <aytung@berkeley.edu>
2023-10-29 06:06:46 -0700

    converted post popup to actual window

--- src/pages/PostPopup.js
+++ src/pages/PostPopup.js
@@ -1,11 +1,13 @@
 import {useState} from 'react';
 import PageTitle from "../PageTitle";
+import PageHeader from '../PageHeader';
+import { placeholder } from '@babel/types';
 
 function PostPopup() {
 
-    const [spotifyURL, setSpotifyURL] = useState("");
+    const [spotifyURL, setSpotifyURL] = useState("Ener");
     const [description, setDescription] = useState("");
-
+    const [playlistname, setPlaylistName] = useState("")
     
     // TODO
     
@@ -18,7 +20,8 @@ function PostPopup() {
             headers: {'Content-Type':'application/json'},
             body: JSON.stringify({
                 "playlistURL": ID,
-                "description": description
+                "description": description,
+                "name": playlistname
             })
     }).then(response => {
         return response;
@@ -29,11 +32,15 @@ function PostPopup() {
 
 
     return (
-        <div className="post-popup">
+
+        <div>
+            <PageHeader></PageHeader>
+
             <PageTitle text="UPLOAD PLAYLIST"></PageTitle>
             <form onSubmit={handleSubmit}>
-                <input className="bordered-elem" id="url" type="text" onChange={(e)=>{setSpotifyURL(e.target.value) }}></input>
-                <textarea className="bordered-elem" id="description" onChange={(e)=>{setDescription(e.target.value)}}></textarea>
+                <input className="bordered-elem" id="playlist-name" type="text" onChange={(e)=>{setPlaylistName(e.target.value)}} placeholder='Name of playlist'></input>
+                <input className="bordered-elem" id="url" type="text" onChange={(e)=>{setSpotifyURL(e.target.value) }} placeholder='Spotify playlist URL'></input>
+                <textarea className="bordered-elem" id="description" onChange={(e)=>{setDescription(e.target.value)}} placeholder='Description of playlist'></textarea>
                 <input type="submit" className="bordered-elem" id="submit-button" value="SUBMIT"></input>
             </form>
         </div>
